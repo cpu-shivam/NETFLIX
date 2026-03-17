17fddbed97824725b44dea4eb238319376300214
cpu-shivam <shivamagg2003@gmail.com>
2026-03-17 23:19:17 +0530

    remove unnecessary blank lines in constants file

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -11,9 +11,6 @@ export const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjVjYjljY2NjOWQyYzQ3MGQ4OTgzODU2YWNiMzRmOCIsIm5iZiI6MTc3MzA3MjgyNS4zNTM5OTk5LCJzdWIiOiI2OWFlZjFiOTNmZTZmYTFhODZjMWNjOTkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ie2Clvj_4BBkmiNeEO3Mn8AHnrDz_RLf80K8eNs5APA",
   },
 };
-
-
-
 export const SUPPORTED_LANG = [
   { identifier: "english", name: "English" },
   { identifier: "hindi", name: "Hindi" },
